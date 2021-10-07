537e2c12429efc1598198d2ab1aedc0d3dd87abb
hussnainjav3d <55350630+hussnainjav3d@users.noreply.github.com>
2021-10-07 13:28:34 +0500

    Adding New Components:

---
+++ src/request.js
@@ -0,0 +1,14 @@
+const API_KEY = "90f8f366200af5d84b15e3c0d22ab1cb";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie/?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie/?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie/?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie/?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie/?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
